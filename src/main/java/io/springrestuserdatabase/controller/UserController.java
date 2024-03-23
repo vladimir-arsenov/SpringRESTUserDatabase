c97ef8e3cade48bf6b916a919ba39400956dbf92
William <arsenovova7@gmail.com>
2024-03-23 21:30:37 +0300

    Methods of the UserController return ResponseEntity now

--- src/main/java/io/springrestuserdatabase/controller/UserController.java
+++ src/main/java/io/springrestuserdatabase/controller/UserController.java
@@ -3,6 +3,8 @@ package io.springrestuserdatabase.controller;
 import io.springrestuserdatabase.exceptions.UserNotFoundException;
 import io.springrestuserdatabase.model.User;
 import io.springrestuserdatabase.repository.UserRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,31 +13,32 @@ import java.util.List;
 @RequestMapping("/users")
 public class UserController {
 
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
     public UserController(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
     @GetMapping
-    public List<User> getAllUsers() {
-        return userRepository.findAll();
+    public ResponseEntity<List<User>> getAllUsers() {
+        return ResponseEntity.ok(userRepository.findAll());
     }
 
     @GetMapping("/{id}")
-    public User getUser(@PathVariable Long id) {
-        return userRepository.findById(id).orElseThrow(() -> new UserNotFoundException(id));
+    public ResponseEntity<User> getUser(@PathVariable Long id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new UserNotFoundException(id));
+        return ResponseEntity.ok(user);
     }
 
     @PostMapping
-    public User addUser(@RequestBody User newUser) {
-        System.out.println(newUser);
-        return userRepository.save(newUser);
+    public ResponseEntity<User> addUser(@RequestBody User newUser)  {
+        return new ResponseEntity<>(userRepository.save(newUser), HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
-    public User editUser(@RequestBody User newUser, @PathVariable Long id) {
-        return userRepository.findById(id).map(user -> {
+    public ResponseEntity<User> editUser(@RequestBody User newUser, @PathVariable Long id) {
+        return ResponseEntity.ok(userRepository.findById(id).map(user -> {
             user.setFirstName(newUser.getFirstName());
             user.setLastName(newUser.getLastName());
             user.setBirthday(newUser.getBirthday());
@@ -43,11 +46,12 @@ public class UserController {
         }).orElseGet(() -> {
             newUser.setId(id);
             return userRepository.save(newUser);
-        });
+        }));
     }
 
     @DeleteMapping("/{id}")
-    public void deleteUser(@PathVariable Long id) {
+    public ResponseEntity<?> deleteUser(@PathVariable Long id) {
         userRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 }

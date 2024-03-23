2a38f322b608faca0e6b0229bcac470fe81e5d09
William <arsenovova7@gmail.com>
2024-03-23 16:06:25 +0300

    Initial commit

---
+++ src/main/java/io/springrestuserdatabase/SpringRestUserDatabaseApplication.java
@@ -0,0 +1,13 @@
+package io.springrestuserdatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestUserDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestUserDatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/springrestuserdatabase/SpringRestUserDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package io.springrestuserdatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestUserDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
